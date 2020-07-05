f2c632f3f195f1150d5e28152a542e6269a51380
Wilson Neto B.R <contato@wilsonneto.com.br>
2020-07-04 23:20:32 -0300

    Update Index.cshtml.cs

--- SiteContagem/Pages/Index.cshtml.cs
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -21,7 +21,7 @@ namespace SiteContagem.Pages
                 TempData["Local"] = _CONTADOR.Local;
                 TempData["Kernel"] = _CONTADOR.Kernel;
                 TempData["TargetFramework"] = _CONTADOR.TargetFramework;
-                TempData["MensagemFixa"] = "Teste 04";
+                TempData["MensagemFixa"] = "Teste 05";
                 TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
             }            
         }
